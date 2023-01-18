5fa5574f0e9246b3d91a7cc87f4ee590361845c4
Andreza <andrezaleal@alu.ufc.br>
2023-01-18 12:15:58 -0300

    upload project

---
+++ README.md
@@ -0,0 +1,15 @@
+## Descrição
+
+Um Mini Sistema de Livraria feito para a Disciplina de POO :3
+
+## Equipe
+
+A UML do projeto foi desenvolvida em conjunto e cada desenvolvedora ficou responsável por codar uma parte do projeto.
+
+Ana Eloise Ferreira Neves (470069)
+Andreza Deyse Leal de Souza (472057)
+
+## Como Executar
+
+- Abra a IDE de sua preferência
+- Execute o arquivo App.java
---
+++ src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Gerente gerente1 = new Gerente("Ben Solo", "567.364.899-32", "Saraiva RioMar");
+        Funcionario funcionario1 = new Funcionario("Olivia Smith", "456.222.645-78", "Saraiva North Shopping");
+        gerente1.baterPonto();
+        funcionario1.baterPonto();
+
+        Cliente cliente1 = new Cliente("Leia", "056.888.423-78", "Rua César Augusto, 72", 200);
+        new Venda("É assim que acaba", "Colleen Hoover", 34, cliente1, funcionario1);
+        new Venda("É assim que começa", "Colleen Hoover", 45, cliente1, funcionario1);
+
+        Fornecedor fornecedor1 = new Fornecedor("Cézar Atacado");
+        gerente1.contactarFornecedor("Star Wars: Estrelas Perdidas", "Claudia Gray", 10, fornecedor1);
+        
+    }
+}
